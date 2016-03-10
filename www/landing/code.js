de36dfb368986cfcc8cfa5fa286e4582c11e554e
Robert M Ochshorn <rmo@NUMM.ORG>
2016-03-10 15:17:25 +0100

    refactor chat loop

--- www/landing/code.js
+++ www/landing/code.js
@@ -9,7 +9,7 @@ var dbs = {};
 var msgs ={};
 var views={};
 
-for(var key in db_key) {
+Object.keys(db_key).forEach(function(key) {
     dbs[key] = new S.Database(db_key[key] + "/db/");
     dbs[key].connect();
 
@@ -20,102 +20,121 @@ for(var key in db_key) {
     // Clear
     $list.innerHTML = "";
 
-    views[key] = new S.CollectionView(msgs[key], function(obj, $div) {
-
-	// Are we at the bottom?
-	var at_bottom = ($div.parentElement.scrollTop + $div.parentElement.clientHeight) >= ($div.parentElement.scrollHeight - 30);
-	
-	// replace newlines with breaks
-	if(obj.text) {
-	    var text = obj.text.replace("\n", "<p>");
-	    text = text.replace(/(https?:\/\/[^\s$]*)[\s$]?/gim, '<a class="extlink" target="_blank" href="$1">$1</a> ')
-	    $div.innerHTML = text;
-	}
-	if(obj._attachments) {
-	    for(var name in obj._attachments) {
-		var parts = name.split(".");
-		var ext = parts[parts.length-1].toLowerCase();
-
-		var $a = document.createElement("a");
-		$a.setAttribute("target", "_blank");
-		$a.href = obj.get_attachment_url(name);
-
-		if(ext == "jpg" || ext == "png" || ext == "gif") {
-		    var $img = document.createElement("img");
-		    $img.src = obj.get_attachment_url(name);
-		    $a.appendChild($img);
-		}
-		else {
-		    $a.textContent = name;
-		}
-		$div.appendChild($a);
-	    }
-	}
-
-	if(at_bottom) {
-	    // Make sure we're still at the bottom
-	    $div.parentElement.scrollTop = $div.parentElement.scrollHeight;
-	}
-    }, "li", function(x,y) { return x.time > y.time ? 1 : -1}, $list);
+    views[key] = new S.CollectionView(msgs[key], message_render, "li", message_sort, $list);
 
     // Hook up the input to to be a functioning element
-
     var $textarea = $column.querySelector("textarea");
 
-    var send_message = function() {
-	if(!this.$textarea.value) {
+    // Find the clickable arrow
+    var $arrow = $column.querySelector(".send");
+
+    bind_message_send(dbs[key], $textarea, $arrow);
+    
+    bind_message_drag(dbs[key], $textarea);
+
+    dbs[key].onload = function() {
+	views[key].sort();
+    }
+})
+
+function message_render(obj, $div) {
+    // Are we at the bottom?
+    var at_bottom = ($div.parentElement.scrollTop + $div.parentElement.clientHeight) >= ($div.parentElement.scrollHeight - 30);
+    
+    // replace newlines with breaks
+    if(obj.text) {
+	var text = obj.text.replace("\n", "<p>");
+	text = text.replace(/(https?:\/\/[^\s$]*)[\s$]?/gim, '<a class="extlink" target="_blank" href="$1">$1</a> ')
+	$div.innerHTML = text;
+    }
+    if(obj._attachments) {
+	for(var name in obj._attachments) {
+	    var parts = name.split(".");
+	    var ext = parts[parts.length-1].toLowerCase();
+
+	    var $a = document.createElement("a");
+	    $a.setAttribute("target", "_blank");
+	    $a.href = obj.get_attachment_url(name);
+
+	    if(ext == "jpg" || ext == "png" || ext == "gif") {
+		var $img = document.createElement("img");
+		$img.src = obj.get_attachment_url(name);
+		$a.appendChild($img);
+	    }
+	    else {
+		$a.textContent = name;
+	    }
+	    $div.appendChild($a);
+	}
+    }
+
+    if(at_bottom) {
+	// Make sure we're still at the bottom
+	$div.parentElement.scrollTop = $div.parentElement.scrollHeight;
+    }
+}
+
+function message_sort(x,y) {
+    return x.time > y.time ? 1 : -1
+}
+
+function bind_message_send(db, $textarea, $arrow) {
+
+    var _send_cb = function() {
+	if(!$textarea.value) {
 	    console.log("Refusing to send an empty message");
 	    return
 	}
-	var doc = new S.Document(this.db, {
+	var doc = new S.Document(db, {
 	    "type": "message",
 	    "time": new Date().getTime(),
-	    "text": this.$textarea.value
+	    "text": $textarea.value
 	});
 	doc.save();
 
-	this.$textarea.value = "";
-    }.bind({"$textarea": $textarea, "db": dbs[key]});
+	$textarea.value = "";
+    }
+
+    $arrow.onclick = _send_cb;
 
     $textarea.onkeydown = function(ev) {
 	if(ev.keyCode == 13) {	// Return key
 	    // TODO: Check if SHIFT is depressed, to allow a newline
 	    ev.preventDefault();
 
-	    this.send_message();
+	    _send_cb();
 	}
-    }.bind({"send_message": send_message});
+    }
+    
+}
 
-    // Find the clickable arrow
-    var $arrow = $column.querySelector(".send");
-    $arrow.onclick = send_message;
-
-
+function bind_message_drag(db, $textarea) {
     //Allow dragging files into the text area
+    
     $textarea.addEventListener("dragover", function(evt) {
     	evt.stopPropagation();
     	evt.preventDefault();
     	evt.dataTransfer.dropEffect = "copy";
-	this.$textarea.parentElement.classList.add("over");
-    }.bind({"$textarea": $textarea}), false);
+	$textarea.parentElement.classList.add("over");
+    }, false);
 
     $textarea.addEventListener("dragleave", function(evt) {
     	evt.stopPropagation();
     	evt.preventDefault();
-	this.$textarea.parentElement.classList.remove("over");
-    }.bind({"$textarea": $textarea}), false);
+	$textarea.parentElement.classList.remove("over");
+    }, false)
 
     $textarea.addEventListener("drop", function(evt) {
     	evt.preventDefault();
 
-	if(this.$textarea.parentElement.className.indexOf("over") >= 0) {
-	    this.$textarea.parentElement.classList.remove("over");
+	if($textarea.parentElement.className.indexOf("over") >= 0) {
+	    $textarea.parentElement.classList.remove("over");
 	}
 
 	var files = evt.dataTransfer.files;
 
 	// Make one Document for all uploaded attachments
-	var doc = new S.Document(this.db, {
+	var doc = new S.Document(db, {
 	    "type": "message",
 	    "time": new Date().getTime(),
 	});
@@ -124,30 +143,11 @@ for(var key in db_key) {
 
 	doc.save(function() {
 	    this.doc.put_file_attachment(file.name, this.file, null, function(percent) {
-		console.log("placeholder", "" + Math.floor(percent*100) + "%");
-
-		this.$textarea.setAttribute("placeholder", "" + Math.floor(percent*100) + "%");
+		$textarea.setAttribute("placeholder", "" + Math.floor(percent*100) + "%");
 		if(percent == 1) {
-		    this.$textarea.setAttribute("placeholder", "type or drag here");
+		    $textarea.setAttribute("placeholder", "type or drag here");
 		}
-	    }.bind({"$textarea": this.$textarea}));
-	}.bind({
-	    doc: doc,
-	    "$textarea": this.$textarea,
-	    file: file}));
-
-    }.bind({
-	db: dbs[key],
-	"$textarea": $textarea	
-    }), false);
-
-
-    dbs[key].onload = function() {
-	views[this.key].sort();
-
-	// Scroll to the bottom
-	// this.$list.scrollTop = this.$list.scrollHeight;
-	
-    }.bind({key: key, "$list": $list});
-
+	    });
+	}.bind({doc: doc, file: file}))
+    }, false);
 }
